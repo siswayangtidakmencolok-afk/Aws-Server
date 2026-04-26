6ebd2cd8f5715b1e4b21af017fa1f12d612166ce
fhazwan <siswayangtidakmencolok@gmail.com>
2026-04-26 22:24:16 +0700

    video call navbar baru, dan animasi gelombang (pulse rings) seolah sistem sedang menyambungkan panggilan

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -39,6 +39,12 @@ const routes = [
     meta: { requiresAuth: true },
   },
   {
+    path: '/video-call',
+    name: 'VideoCall',
+    component: () => import('../views/VideoCallView.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
     path: '/:pathMatch(.*)*',
     redirect: '/',
   },
